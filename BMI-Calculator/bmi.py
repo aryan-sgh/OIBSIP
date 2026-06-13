4d2cd5165026232bb9d3ef71d92b486cf67e38c5
aryan-sgh <aspaspirant18@gmail.com>
2026-06-13 19:50:27 +0530

    Update bmi.py

--- BMI-Calculator/bmi.py
+++ BMI-Calculator/bmi.py
@@ -3,4 +3,13 @@ height = float(input("Enter your height in meters: "))
 
 bmi = weight / (height ** 2)
 
-print("Your BMI is:", round(bmi, 2))
+print("\nYour BMI is:", round(bmi, 2))
+
+if bmi < 18.5:
+    print("Category: Underweight")
+elif bmi < 25:
+    print("Category: Normal Weight")
+elif bmi < 30:
+    print("Category: Overweight")
+else:
+    print("Category: Obese")
